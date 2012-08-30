ba678e880efd57b2e0b53fa757de8984a3e168aa
松本 栄二 <matsumotoeiji@matsumoto-no-MacBook-Pro.local>
2012-08-30 22:30:50 +0900

    add detail to summary view

--- iOSsampleApp1/emattsanSummaryViewController.m
+++ iOSsampleApp1/emattsanSummaryViewController.m
@@ -30,8 +30,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    titleView.text = [[events objectAtIndex:row] objectForKey:@"title"];
-    catchView.text = [[events objectAtIndex:row] objectForKey:@"catch"];
 }
 
 - (void)viewDidUnload
@@ -53,6 +51,83 @@
     detail.description = [[events objectAtIndex:row] objectForKey:@"description"];
     
     NSLog(@"%@", detail.eventTitle);
+}
+
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if(section == 0)
+    {
+        return 5;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if(indexPath.section == 0)
+    {
+        static NSString *CellIdentifier = @"Cell";
+
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+
+        if(cell == nil)
+        {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier];
+        }
+        
+        switch(indexPath.row)
+        {
+        case 0:
+            cell.textLabel.text = [[events objectAtIndex:row] objectForKey:@"title"];
+            break;
+
+        case 1:
+            cell.textLabel.text = [[events objectAtIndex:row] objectForKey:@"catch"];
+            break;
+
+        case 2:
+            cell.textLabel.text       = @"定員";
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%@", [[events objectAtIndex:row] objectForKey:@"limit"]];
+            break;
+
+        case 3:
+            cell.textLabel.text       = @"参加者数";
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%@", [[events objectAtIndex:row] objectForKey:@"accepted"]];
+            break;
+            
+        case 4:
+            cell.textLabel.text       = @"補欠者数";
+            cell.detailTextLabel.text = [NSString stringWithFormat:@"%@", [[events objectAtIndex:row] objectForKey:@"waiting"]];
+            break;
+
+        default:
+            break;
+        }
+        return cell;
+    }
+    else
+    {
+        static NSString *CellIdentifier = @"Detail";
+        
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        
+        if(cell == nil)
+        {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:CellIdentifier];
+        }
+
+        cell.textLabel.text = @"詳細";
+        
+        return cell;
+    }
 }
 
 @end
